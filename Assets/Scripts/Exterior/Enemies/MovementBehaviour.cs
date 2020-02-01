df6e23c1ca932d3ef9d9b379da3238d06905488f
Mike1072 <mike1072@users.noreply.github.com>
2020-02-01 15:12:09 -0800

    Refactor enemies and spawning

---
+++ Assets/Scripts/Exterior/Enemies/MovementBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    [SerializeField] private Vector2 direction = Vector2.down;
+
+    Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        rigidbody2D.AddForce(speed * direction);
+    }
+}
