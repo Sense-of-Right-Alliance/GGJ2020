5455f2fe2b7e6de2bc0146d4c148c996d5de87ac
Mike1072 <mike1072@users.noreply.github.com>
2020-02-01 16:33:27 -0800

    Add wave system and Flying Vs

---
+++ Assets/Scripts/Exterior/Enemies/Wave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Wave
+{
+    public List<WaveEvent> WaveEvents { get; }
+
+    public Wave(List<WaveEvent> waveEvents)
+    {
+        WaveEvents = waveEvents;
+    }
+}
