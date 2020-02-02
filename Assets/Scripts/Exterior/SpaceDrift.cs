3f76576894cdbad0eabe284894188d2a1e36a6a3
NarPar <n.pettyjohn@gmail.com>
2020-02-01 16:01:23 -0800

    Ships drop power ups & merged bounds/spawn changes
    Ships drop power ups & merged bounds/spawn changes
    
    Added controller inputs

---
+++ Assets/Scripts/Exterior/SpaceDrift.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpaceDrift : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector2 newPos = transform.position;
+        newPos.y -= speed * Time.deltaTime;
+
+        transform.position = newPos;
+    }
+}
