9fa36719699aa67e4aa457881a4ffc555e0859cd
NarPar <n.pettyjohn@gmail.com>
2020-02-19 22:37:51 -0800

    Such a tool
    Interior player can pick up a fire extinguisher tool and turn it on with R or B Button. This must be used to put out fires! Can't smother them anymore.
    
    Held items are held in front of the player in the direction they're moving.

---
+++ Assets/Scripts/Interior/Tools/Tool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Tool : MonoBehaviour
+{
+    protected bool on = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdateTool();
+    }
+
+    protected virtual void UpdateTool()
+    {
+
+    }
+
+    public virtual void ToggleOn()
+    {
+        on = !on;
+
+        Debug.Log("Tool toggled to " + on.ToString());
+    }
+
+    public virtual void SetOn(bool on)
+    {
+        if (this.on != on)
+        {
+            Debug.Log("Toggling On to " + on);
+            this.on = on;
+        }
+    }
+
+    public virtual void Pickup()
+    {
+        
+    }
+
+    public virtual void Drop()
+    {
+        if (on) on = false;
+    }
+}
